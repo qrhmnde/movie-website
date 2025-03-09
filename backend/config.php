7d010d43f9776fb56594fdb343fbfa851173cb1f
Sorya Aryan <sorya@Sorya.Aryan>
2025-03-09 20:02:21 +0530

    Added backend

---
+++ backend/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Telegram Bot Configuration
+define('BOT_TOKEN', '7037737616:AAHD2_9VCcnx8xpYOJsmrFn141SxZyyTW3w');  // Aapka bot token
+define('CHAT_ID', '1002365529156');  // Aapka channel ID
+
+define('API_URL', "https://api.telegram.org/bot" . BOT_TOKEN . "/");
+
+?>
